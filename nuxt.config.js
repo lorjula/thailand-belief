8eba4ed84f77c73ba0e15da694ece740d795bd6b
prongbang <prongbang@gmail.com>
2021-06-21 23:17:14 +0700

    Config css and js to vendor

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 const routerBase = {
   router: {
-    base: '/thailand-belief/'
+    base: '/thbelief/'
   }
 };
 
@@ -13,37 +13,33 @@ export default {
   head: {
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {rel: 'stylesheet', type: 'text/css', href: '/css/bootstrap.min.css'},
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
     ],
-    script: [
-      {src: "/js/bootstrap.bundle.min.js"},
-    ]
+    script: []
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
   /*
   ** Global CSS
   */
   css: [
+    "~/assets/css/bootstrap.min.css",
   ],
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
   /*
   ** Nuxt.js dev-modules
   */
-  buildModules: [
-  ],
+  buildModules: [],
   /*
   ** Nuxt.js modules
   */
@@ -56,16 +52,18 @@ export default {
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
-  axios: {
-  },
+  axios: {},
   /*
   ** Build configuration
   */
   build: {
+    vendor: [
+      '~/assets/js/bootstrap.bundle.min.js'
+    ],
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
